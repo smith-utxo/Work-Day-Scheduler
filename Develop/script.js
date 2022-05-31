00ce6f2e8d4cf726ca1edbe4a0a6ec97f7015c84
Adam Smith <smith.adam937@gmail.com>
2022-05-31 08:48:33 -0500

    retrieve local storage and display on calendar

--- Develop/script.js
+++ Develop/script.js
@@ -19,12 +19,19 @@ $(document).ready(function() {
     console.log(userText);
     console.log(hour);
 
-  localStorage.setItem(userText, hour); 
+    //Save userInput to local storage 
+  localStorage.setItem(hour, userText); 
 
-});
+  });
 
-
-});
-
-
-
+  //Get Calendar inputs from local storage if any exist 
+  $("#hour1 .description").val(localStorage.getItem("hour1"));
+  $("#hour2 .description").val(localStorage.getItem("hour2"));
+  $("#hour3 .description").val(localStorage.getItem("hour3"));
+  $("#hour4 .description").val(localStorage.getItem("hour4"));
+  $("#hour5 .description").val(localStorage.getItem("hour5"));
+  $("#hour6 .description").val(localStorage.getItem("hour6"));
+  $("#hour7 .description").val(localStorage.getItem("hour7"));
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+})
