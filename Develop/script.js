0f7b8daf49f77a64ce4ac542f5c4b6e71723fe87
Adam Smith <smith.adam937@gmail.com>
2022-05-30 21:12:49 -0500

    Save to localStorage function add

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,30 @@
+// Time variables
 var today = moment().format('dddd, MMMM DD'); 
 var currentHour = moment().format('h a');
 var displayDay = document.getElementById('currentDay');
 
-
-
-
-console.log(currentHour);
 displayDay.innerText = today; 
 
-/*var auditHour = function(){
-  if (currentHour == );
-}
 
-auditHour(); */
+//Save Button event Listener 
+var save = document.querySelectorAll('.saveBtn')
+
+// Required syntax to use jquery 
+$(document).ready(function() {
+    
+  //event listener in jquery.
+  $(save).on('click', function() {
+    var userText = $(this).siblings(".description").val(); 
+    var hour = $(this).parent().attr('id'); 
+    console.log(userText);
+    console.log(hour);
+
+  localStorage.setItem(userText, hour); 
+
+});
+
+
+});
+
+
+
